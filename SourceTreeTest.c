8b17356ae628ad1f463c92cbce6ffd1d89ed084c
junseok5bba <quasi0912@gmail.com>
2019-04-10 16:46:41 +0900

    수정

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -5,15 +5,12 @@ int main()
   int i=0;
 
   prtinf("First Oss Code\n");
-<<<<<<< HEAD
-  printf("Second Oss Code
+  printf("Second Oss Code");
   printf("마스터 브랜치에 코드를 추가하였음");
-=======
   printf("Second Oss Code");
 
   for(int i=0;i<10;i++){
     printf("Test 브랜치\n");
   }
->>>>>>> Test
   return 0;
 }
